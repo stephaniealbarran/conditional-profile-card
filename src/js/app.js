033153c812fdf3b782b266b2db7a86df0ed5ed41
stephaniealbarran <stefanyalbarran@hotmail.com>
2022-06-05 17:40:41 +0000

    actualizado

--- src/js/app.js
+++ src/js/app.js
@@ -41,16 +41,16 @@ function render(variables = {}) {
     variables.country === null ? "" : variables.country
   } </h3>
           <ul class=" ${variables.socialMediaPosition} ">
-            <li><a href=" ${
+            <li><a href="https://twitter.com/${
               variables.twitter
             } "><i class="fab fa-twitter"></i></a></li>
-            <li><a href=" ${
+            <li><a href="https://github.com/${
               variables.github
             } "><i class="fab fa-github"></i></a></li>
-            <li><a href=" ${
+            <li><a href="https://linkedin.com/${
               variables.linkedin
             } "><i class="fab fa-linkedin"></i></a></li>
-            <li><a href=" ${
+            <li><a href=""https://instagram.com/${
               variables.instagram
             } "><i class="fab fa-instagram"></i></a></li>
           </ul>
